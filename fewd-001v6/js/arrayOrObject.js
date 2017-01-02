9da247bbb9d5ee1887214a549c813db8820c70ae
ricmclaughlin <ric@mclaughlin.today>
2017-01-01 19:40:50 -0800

    arrayOrObject.js

---
+++ fewd-001v6/js/arrayOrObject.js
@@ -0,0 +1,16 @@
+// use an array for LIFO, FIFO
+
+// use an array to store an ordered list
+
+
+
+
+// don't use an object to store numbered keys
+// bad
+var myObjectThatShouldBeAnArray = {
+  1: 'cat',
+  2: 'dog'
+};
+
+//good
+var myArray = ['cat', 'dog'];
